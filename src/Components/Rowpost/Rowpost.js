0886ed3960221be4ae82b15e38e8e674f84bd504
Dileepmc <dileepmc51@gmail.com>
2024-08-21 21:03:13 +0530

    second commit

--- src/Components/Rowpost/Rowpost.js
+++ src/Components/Rowpost/Rowpost.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import "./Rowpost.css"
 
 function Rowpost(props) {
     return (
         <div className='row'>
-            <h2>title</h2>
+            <h2>Title</h2>
             <div className='posters'>
-                <img src= 'https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
+                <img className='poster' src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLps1-xf4Q4T6YqjUokErcQv7LUnRkkGd6Lg&s' alt="movie poster" />
 
+              
             </div>
         </div>
     );
